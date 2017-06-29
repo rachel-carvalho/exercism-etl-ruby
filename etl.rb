60ea8e50c12ce22f7aae59dabcf786cb772c59f3
Rachel Carvalho <rachel.carvalho@gmail.com>
2017-06-29 12:28:16 -0400

    use map/flatten instead of reduce

--- etl.rb
+++ etl.rb
@@ -4,9 +4,6 @@ end
 
 class ETL
   def self.transform(original)
-    original.reduce([]) do |all, items|
-      score, letters = items
-      all + letters.map { |letter| [letter.downcase, score] }
-    end.to_h
+    original.map { |score, letters| letters.map { |letter| [letter.downcase, score] } }.flatten(1).to_h
   end
 end
