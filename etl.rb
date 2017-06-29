ac9ee949c63742695b0a24f3539d557032008a94
Rachel Carvalho <rachel.carvalho@gmail.com>
2017-06-29 13:08:29 -0400

    extract assign_scores

--- etl.rb
+++ etl.rb
@@ -4,6 +4,11 @@ end
 
 class ETL
   def self.transform(original)
-    original.map { |score, letters| letters.map { |letter| [letter.downcase, score] } }.flatten(1).to_h
+    original.map(&method(:assign_scores)).flatten(1).to_h
+  end
+
+  def self.assign_scores(items)
+    score, letters = items
+    letters.map { |letter| [letter.downcase, score] }
   end
 end
