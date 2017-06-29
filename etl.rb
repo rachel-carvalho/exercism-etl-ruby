13f3dfb522b7e860f608536a21b933fedf96989f
Rachel Carvalho <rachel.carvalho@gmail.com>
2017-06-29 13:26:56 -0400

    fix product call (that's not how it works, duh)

--- etl.rb
+++ etl.rb
@@ -4,6 +4,6 @@ end
 
 class ETL
   def self.transform(original)
-    original.flat_map { |score, letters| letters.map(&:downcase).product([score] * letters.length) }.to_h
+    original.flat_map { |score, letters| letters.map(&:downcase).product([score]) }.to_h
   end
 end
