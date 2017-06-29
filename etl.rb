9f0687e157d14c46b18ff5923d266eb50f5e426a
Rachel Carvalho <rachel.carvalho@gmail.com>
2017-06-29 13:21:06 -0400

    use product because why not

--- etl.rb
+++ etl.rb
@@ -4,6 +4,6 @@ end
 
 class ETL
   def self.transform(original)
-    original.flat_map { |score, letters| letters.map { |letter| [letter.downcase, score] } }.to_h
+    original.flat_map { |score, letters| letters.map(&:downcase).product([score] * letters.length) }.to_h
   end
 end
